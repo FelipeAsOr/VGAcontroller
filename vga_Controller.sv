9cc2bcfde25a2b0128c4a770050c74ae669d01d5
Andrew I M <65468553+Tuzcar@users.noreply.github.com>
2020-05-28 18:59:21 -0500

    Added comments and edited variables
    These comments and variables are for a better understanding of the code using our model as reference.

--- vga_Controller.sv
+++ vga_Controller.sv
@@ -1,44 +1,50 @@
-module hvsync_generator(
-    input clk,
+module Vga_controller(
+    input pxclk, // Must be 25.175 MHz, which is around 40 ns
     output vga_h_sync,
     output vga_v_sync,
-    output reg inDisplayArea,
-    output reg [9:0] CounterX,
-    output reg [8:0] CounterY
+    output reg Display,
+    output reg [9:0] Column,
+    output reg [8:0] Row
   );
     reg vga_HS, vga_VS;
 
-    wire CounterXmaxed = (CounterX == 800); // 16 + 48 + 96 + 640
-    wire CounterYmaxed = (CounterY == 525); // 10 + 2 + 33 + 480
+    // It is defined base on the specifications for a 640 x 480 display with 60Hz
+    // Refresh rate
+    wire ColumnMaxed = (Column == 800); // 16 + 48 + 96 + 640
+    wire RowMaxed = (Row == 525); // 10 + 2 + 33 + 480
 
-    always @(posedge clk)
-    if (CounterXmaxed)
-      CounterX <= 0;
+    // These blocks allow for the fullfilment of the columns and the rows
+    always @(posedge pxclk)
+    if (ColumnMaxed)
+      Column <= 0;
     else
-      CounterX <= CounterX + 1;
+      Column <= Column + 1;
 
-    always @(posedge clk)
+    // The rows begin when the column finishes.
+    always @(posedge pxclk)
     begin
-      if (CounterXmaxed)
+      if (ColumnMaxed)
       begin
-        if(CounterYmaxed)
-          CounterY <= 0;
+        if(RowMaxed)
+          Row <= 0;
         else
-          CounterY <= CounterY + 1;
+          Row <= Row + 1;
       end
     end
 
-    always @(posedge clk)
+    // This is the block responsible for the sync pulse. 
+    always @(posedge pxclk)
     begin
-      vga_HS <= (CounterX > (640 + 16) && (CounterX < (640 + 16 + 96)));   // active for 96 clocks
-      vga_VS <= (CounterY > (480 + 10) && (CounterY < (480 + 10 + 2)));   // active for 2 clocks
+      vga_HS <= (Column > (640 + 16) && (Column < (640 + 16 + 96)));   // active for 96 clocks
+      vga_VS <= (Row > (480 + 10) && (Row < (480 + 10 + 2)));   // active for 2 clocks
     end
 
-    always @(posedge clk)
+    always @(posedge pxclk) //This is the time where an image will show.
     begin
-        inDisplayArea <= (CounterX < 640) && (CounterY < 480);
+        Display <= (Column < 640) && (Row < 480);
     end
 
+    // We assign to the syncs the sync pulse in order of the time.
     assign vga_h_sync = ~vga_HS;
     assign vga_v_sync = ~vga_VS;
 
