a355a49bf9c85ae5f6e1da4f0f737edf91452902
Andrew I M <65468553+Tuzcar@users.noreply.github.com>
2020-05-28 19:17:16 -0500

    Added ImageSource
    This will allows us to simulate and image, it's based on an image one can create in FPGA, the purpose is to generate 9 blocks in the display, and each will show a different combination of the RGB colours (Some repeated, obviously), and later it will serve to check if the registers are correct.

---
+++ ImageSource.sv
@@ -0,0 +1,39 @@
+module ImageSource(
+    input pxclk,
+    output reg [7:0] red,
+    output reg [7:0] green,
+    output reg [7:0] blue,
+    output wire hsync_out,
+    output wire vsync_out
+ );
+    wire inDisplayArea;
+    wire [9:0] Column;
+    wire [9:0] Row;
+
+    Vga_controller hvsync(
+      .clk(pxclk),
+      .vga_h_sync(hsync_out),
+      .vga_v_sync(vsync_out),
+      .Column(Column),
+      .Row(Row),
+      .inDisplayArea(inDisplayArea)
+    );
+
+    always @(posedge pxclk)
+    begin
+      if (inDisplayArea) begin
+        if(Column < 214) begin
+          if (row < 160) begin
+            red <= 255;
+            green <= 0;
+            blue <= 0;
+          end
+        end
+      else begin // If it's not to display, go dark
+        red <= 0;
+        greeen <= 0;
+        blue <= 0;
+      end
+    end
+
+endmodule
