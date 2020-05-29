d275dfafb843ffcb4bcf53596f4a5b5496e72c47
Andrew I M <65468553+Tuzcar@users.noreply.github.com>
2020-05-29 17:33:36 -0500

    Version 1.0 of VGA Controller
    There were errors because Column and Row were not initialized at 0, so an initial begin was added. Also, Row wasn't refreshing, so instead of its own block, column and row now share the same always block, this solved the problem.

--- vga_Controller.sv
+++ vga_Controller.sv
@@ -4,9 +4,14 @@ module Vga_controller(
     output vga_v_sync,
     output reg Display,
     output reg [9:0] Column,
-    output reg [8:0] Row
+    output reg [9:0] Row
   );
     reg vga_HS, vga_VS;
+
+    initial begin
+      Column <= 0;
+      Row <= 0;
+    end
 
     // It is defined base on the specifications for a 640 x 480 display with 60Hz
     // Refresh rate
@@ -14,35 +19,27 @@ module Vga_controller(
     wire RowMaxed = (Row == 525); // 10 + 2 + 33 + 480
 
     // These blocks allow for the fullfilment of the columns and the rows
-    always @(posedge pxclk)
-    if (ColumnMaxed)
-      Column <= 0;
-    else
-      Column <= Column + 1;
-
-    // The rows begin when the column finishes.
-    always @(posedge pxclk)
-    begin
-      if (ColumnMaxed)
-      begin
-        if(RowMaxed)
+    always @(posedge pxclk) begin
+    Display <= (Column < 640) && (Row < 480);
+    if (ColumnMaxed) begin
+        if(RowMaxed)   // The rows begin when the column finishes.
           Row <= 0;
         else
           Row <= Row + 1;
-      end
+        Column <= 0;
+    end
+    else
+      Column <= Column + 1;
     end
 
-    // This is the block responsible for the sync pulse. 
+
+    // This is the block responsible for the sync pulse.
     always @(posedge pxclk)
     begin
-      vga_HS <= (Column > (640 + 16) && (Column < (640 + 16 + 96)));   // active for 96 clocks
-      vga_VS <= (Row > (480 + 10) && (Row < (480 + 10 + 2)));   // active for 2 clocks
+      vga_HS <= (Column > (640 + 16) && (Column <= (640 + 16 + 96)));   // active for 96 clocks
+      vga_VS <= (Row > (480 + 10) && (Row <= (480 + 10 + 2)));   // active for 2 clocks
     end
 
-    always @(posedge pxclk) //This is the time where an image will show.
-    begin
-        Display <= (Column < 640) && (Row < 480);
-    end
 
     // We assign to the syncs the sync pulse in order of the time.
     assign vga_h_sync = ~vga_HS;
