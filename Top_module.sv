4822f65b4fadf2e490031f63049dc5478dc5374d
Andrew I M <65468553+Tuzcar@users.noreply.github.com>
2020-05-29 17:31:08 -0500

    Version 1.0 of TopModule
    All modules and programs for the VGA Controller are now connected.

--- Top_module.sv
+++ Top_module.sv
@@ -6,9 +6,47 @@ module Top_module();
 bit clk;
 bit pxclk;
 
+wire [9:0] Column, Row;
+wire [7:0] red, blue, green;
+wire hsync_out, vsync_out;
+wire Display;
+
 PixelClock duv(
   .clk(clk),
   .pxclk(pxclk)
+  );
+
+Vga_controller hvsync(
+  .pxclk(pxclk),
+  .vga_h_sync(hsync_out),
+  .vga_v_sync(vsync_out),
+  .Column(Column),
+  .Row(Row),
+  .Display(Display)
+  );
+
+ImageSource image(
+  .pxclk(pxclk),
+  .Display(Display),
+  .Column(Column),
+  .Row(Row),
+  .red(red),
+  .green(green),
+  .blue(blue),
+  .hsync_out(hsync_out),
+  .vsync_out(vsync_out)
+  );
+
+Testbench tb(
+  .pxclk(pxclk),
+  .Display(Display),
+  .Column(Column),
+  .Row(Row),
+  .red(red),
+  .green(green),
+  .blue(blue),
+  .hsync_out(hsync_out),
+  .vsync_out(vsync_out)
   );
 
 always
